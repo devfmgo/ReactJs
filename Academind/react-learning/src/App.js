77825e8ba7ce7de413971cf3522667fc74a12131
devfm <devfm.go@gmail.com>
2020-12-18 22:12:18 +0700

    react-learning

---
+++ Academind/react-learning/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Person from "./Person/Person";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'am React App</h1>
+      <p>it's realy working</p>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ Academind/react-learning/src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from "react";
+const person = () => {
+  return <p>I'am a {Math.floor(Math.random() * 30)} years old Person!</p>;
+};
+
+export default person;
