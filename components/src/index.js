16896eafc72858e266cb693451873a00e71004c9
devfm <devfm.go@gmail.com>
2020-12-23 18:50:51 +0700

    props chlid

--- components/src/index.js
+++ components/src/index.js
@@ -2,27 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        content="Nice blog post"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Fikri"
-        timeAgo="Today at 5:45 PM"
-        content="Nice  and great blog post"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Gita"
-        timeAgo="Today at 6:45 PM"
-        content="Nice your blog post"
-        avatar={faker.image.image()}
-      />
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+                Are you sure want to do this ?
+            </div>
+        </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Fikri"
+          timeAgo="Today at 5:45 PM"
+          content="Nice  and great blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Gita"
+          timeAgo="Today at 6:45 PM"
+          content="Nice your blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
