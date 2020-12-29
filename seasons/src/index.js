179a3aadd32efa9fb20e3e0ed250a4babdaabe3e
devfm <devfm.go@gmail.com>
2020-12-29 11:24:50 +0700

    create simple render if

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,22 +1,31 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            lat:null,
-        };
-    }
-    render() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState 
-                this.setState({lat:position.coords.latitude});
-            },
-            (err) => console.log(err)
-          );
-          return <div>Latitude: {this.state.lat}</div>;
+      (position) => {
+        // we called setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>;
     }
- 
+    if  (!this.state.errorMessage && this.state.lat)  {
+      return <div>Latitude: {this.state.lat}</div>;;
+    }
+    return <div>Loading..!</div>
+  }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
