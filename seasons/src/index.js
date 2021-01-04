b733ca6e5665ae4dcc8f47be97d4f1e540c17640
devfm <devfm.go@gmail.com>
2021-01-04 16:51:36 +0700

    add props & add SeasonDisplay.css

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,30 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       lat: null,
       errorMessage: "",
     };
-        window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
+    componentDidMount(){
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          // we called setState
+          this.setState({ lat: position.coords.latitude });
+        },
+        err => {
+          this.setState({ errorMessage: err.message });
+        }
+      );
+    }
+       
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error : {this.state.errorMessage}</div>;
     }
     if  (!this.state.errorMessage && this.state.lat)  {
-      return <div>Latitude: {this.state.lat}</div>;;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading..!</div>
   }
