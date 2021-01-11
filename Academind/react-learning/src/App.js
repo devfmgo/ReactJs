b0e8ea2be04bc11f30d0ac573ca4065d8bb9d8b3
devfm <devfm.go@gmail.com>
2021-01-11 21:11:57 +0700

    add folder learning

--- Academind/react-learning/src/App.js
+++ Academind/react-learning/src/App.js
@@ -7,32 +7,32 @@ class App extends Component {
       { name: "Fikri", age: 26 },
       { name: "Gita", age: 25 },
     ],
-    otherState:'some other value'
+    otherState: "some other value",
   };
-  switchNameHandler = ()=>{
-    // console.log('Was Clicked')
+  switchNameHandler = () => {
     // Don't do this this.state.persons[0].name = 'Muhamad Fikri';
     this.setState({
-      persons:[
-      { name: "Muhamad Fikri", age: 26 },
-      { name: "Gita", age: 25 },
-    ]})
-  }
+      persons: [
+        { name: "Muhamad Fikri", age: 26 },
+        { name: "Gita", age: 25 },
+      ],
+    });
+  };
   render() {
     return (
       <div className="App">
-          <h1>Hi, I'am React App</h1>
-          <p>it's realy working</p>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-          />
-        </div>
+        <h1>Hi, I'am React App</h1>
+        <p>it's realy working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+      </div>
     );
   }
 }
